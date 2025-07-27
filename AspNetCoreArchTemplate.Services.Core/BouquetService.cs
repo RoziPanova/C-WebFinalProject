22814ca336d5a7c41e9aabb03daaa64f0e771614
LazyPottatto <rozi04@abv.bg>
2025-07-27 20:35:54 +0300

    Added Bouquet service and the implementation of the interface

---
+++ AspNetCoreArchTemplate.Services.Core/BouquetService.cs
@@ -0,0 +1,43 @@
+﻿namespace AspNetCoreArchTemplate.Services.Core
+{
+    using AspNetCoreArchTemplate.Data.Repository.Interfaces;
+    using AspNetCoreArchTemplate.Services.Core.Interfaces;
+    using AspNetCoreArchTemplate.Web.ViewModels.Bouquet;
+    using Microsoft.EntityFrameworkCore;
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using static AspNetCoreArchTemplate.GCommon.ApplicationConstants;
+
+    public class BouquetService : IBouquetService
+    {
+        private readonly IBouquetRepository bouquetRepository;
+
+        public BouquetService(IBouquetRepository bouquetRepository)
+        {
+            this.bouquetRepository = bouquetRepository;
+        }
+        public async Task<IEnumerable<BouquetIndexViewModel>> GetAllBouquetsAsync()
+        {
+
+            IEnumerable<BouquetIndexViewModel> allBouquets = await this.bouquetRepository
+                .GetAllAttached()
+                .AsNoTracking()
+                .Select(b => new BouquetIndexViewModel()
+                {
+                    Id = b.Id.ToString(),
+                    Name = b.Name,
+                    ImageUrl = b.ImageUrl,
+                })
+                .ToListAsync();
+            foreach (BouquetIndexViewModel bouquet in allBouquets)
+            {
+                if (String.IsNullOrEmpty(bouquet.ImageUrl))
+                {
+                    bouquet.ImageUrl = $"{NoImageUrl}";
+                }
+            }
+            return allBouquets;
+        }
+    }
+}
+
---
+++ AspNetCoreArchTemplate.Services.Core/Interfaces/IBouquetService.cs
@@ -0,0 +1,9 @@
+﻿namespace AspNetCoreArchTemplate.Services.Core.Interfaces
+{
+    using AspNetCoreArchTemplate.Web.ViewModels.Bouquet;
+
+    public interface IBouquetService
+    {
+        Task<IEnumerable<BouquetIndexViewModel>> GetAllBouquetsAsync();
+    }
+}
