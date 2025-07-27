e4dbf9b8b91540816fe041da03ed6febd94eaf7c
LazyPottatto <rozi04@abv.bg>
2025-07-27 20:21:59 +0300

    Added repository implemented during the workshop with Kris, and implemented it for the bouquets

---
+++ AspNetCoreArchTemplate.Data/Repository/BouquetRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace AspNetCoreArchTemplate.Data.Repository
+{
+    using AspNetCoreArchTemplate.Data.Models;
+    using AspNetCoreArchTemplate.Data.Repository.Interfaces;
+    using CinemaApp.Data.Repository;
+
+    public class BouquetRepository :
+        BaseRepository<Bouquet, Guid>, IBouquetRepository
+    {
+        public BouquetRepository(ApplicationDbContext dbContext)
+            : base(dbContext)
+        {
+
+        }
+    }
+}
---
+++ AspNetCoreArchTemplate.Data/Repository/Interfaces/IBouquetRepository.cs
@@ -0,0 +1,10 @@
+﻿using AspNetCoreArchTemplate.Data.Models;
+
+namespace AspNetCoreArchTemplate.Data.Repository.Interfaces
+{
+    public interface IBouquetRepository :
+        IRepository<Bouquet, Guid>, IAsyncRepository<Bouquet, Guid>
+    {
+
+    }
+}
