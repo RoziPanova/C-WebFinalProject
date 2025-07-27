2b902f58e035f88f94bdb4502569bf958a5bc205
LazyPottatto <rozi04@abv.bg>
2025-07-27 20:38:31 +0300

    Created the ViewModel necessary for the IndexView

---
+++ AspNetCoreArchTemplate.Web.ViewModels/Bouquet/BouquetIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCoreArchTemplate.Web.ViewModels.Bouquet
+{
+    public class BouquetIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+
+    }
+}
