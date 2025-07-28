3f0c7fb43976f6b61e3bc18b5c18871ed80f0e42
LazyPottatto <rozi04@abv.bg>
2025-07-28 21:32:18 +0300

    Abstracted the shared view models into a combined ProuctViewModels and made the index and details views pariatViews for easier code reuse

---
+++ AspNetCoreArchTemplate.Web.ViewModels/Products/ProductDetailsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AspNetCoreArchTemplate.Web.ViewModels.Products
+{
+    public class ProductDetailsViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? Description { get; set; }
+        public decimal Price { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
---
+++ AspNetCoreArchTemplate.Web.ViewModels/Products/ProductIndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace AspNetCoreArchTemplate.Web.ViewModels.Products
+{
+    public class ProductIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string? ImageUrl { get; set; }
+
+    }
+}
