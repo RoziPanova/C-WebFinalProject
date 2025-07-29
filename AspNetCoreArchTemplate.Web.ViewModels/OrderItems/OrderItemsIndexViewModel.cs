e4001724aad249bf2110a18061b3db90745c54df
LazyPottatto <rozi04@abv.bg>
2025-07-30 00:08:45 +0300

    Created an ViewModel for the new Additions to the project and adjusted the Details model to have the event type for the arrangements becuase i missed to implement them yesterday

---
+++ AspNetCoreArchTemplate.Web.ViewModels/OrderItems/OrderItemsIndexViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace AspNetCoreArchTemplate.Web.ViewModels.OrderItems
+{
+    public class OrderItemsIndexViewModel
+    {
+        public string Id { get; set; } = null!;
+        public string Name { get; set; } = null!;
+        public string Type { get; set; } = null!;
+        public string ImageUrl { get; set; } = null!;
+        public int Quantity { get; set; }
+        public string? Price { get; set; }
+        public decimal Total { get; set; }
+    }
+}
--- AspNetCoreArchTemplate.Web.ViewModels/Products/ProductDetailsViewModel.cs
+++ AspNetCoreArchTemplate.Web.ViewModels/Products/ProductDetailsViewModel.cs
@@ -5,6 +5,7 @@
         public string Id { get; set; } = null!;
         public string Name { get; set; } = null!;
         public string? Description { get; set; }
+        public string? EventType { get; set; }
         public decimal Price { get; set; }
         public string? ImageUrl { get; set; }
     }
